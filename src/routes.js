dcf185af3c235ea0fbe11a94bb3e21105a693866
robsu17 <robson.costa134@gmail.com>
2023-05-29 11:54:21 -0300

    Completo
    Conceitos fundamentais de node

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,11 @@ export const routes = [
     method: 'GET',
     path: BuildRoutePath('/users'),
     handler: (req, res) => {
-      const users = database.select('users')
+      const { search } = req.query
+      const users = database.select('users', search ? {
+        name: search,
+        email: search,
+      } : null)
       return res.end(JSON.stringify(users))
     }
   },
@@ -33,7 +37,22 @@ export const routes = [
     method: 'DELETE',
     path: BuildRoutePath('/users/:id'),
     handler: (req, res) => {
-      return res.end()
+      const { id } = req.params
+      database.delete('users', id)
+      return res.writeHead(204).end()
+    },
+  },
+  {
+    method: 'PUT',
+    path: BuildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { name, email } = req.body
+      database.update('users', id, {
+        name,
+        email,
+      })
+      return res.writeHead(204).end()
     },
   }
 ]
--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -2,9 +2,9 @@
 
 export function BuildRoutePath(path) {
   const routeParametersRegex = /:([a-zA-Z]+)/g
-  const pathWithParams = path.replaceAll(routeParametersRegex, '(?<id>[a-z0-9\-_])+')
+  const pathWithParams = path.replaceAll(routeParametersRegex, '(?<id>[a-z0-9\-_]+)')
 
-  const pathRegex = new RegExp(`^${pathWithParams}`)
+  const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`)
 
   return pathRegex
 }
